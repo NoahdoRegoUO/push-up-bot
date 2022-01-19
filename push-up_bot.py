1e75ef86a82779ebb24e847a290b601b72fcb920
Noah do Régo <90425027+NoahdoRegoUO@users.noreply.github.com>
2022-01-19 02:33:12 +0000

    !added database for total push-ups

--- push-up_bot.py
+++ push-up_bot.py
@@ -7,19 +7,23 @@ import typing
 #import random
 #import requests
 
+# Extra discord libraries
 from discord import message
 from discord.ext import commands
 
+# Replit database
+from replit import db
+
+# Web server hosting function
 from keep_alive import keep_alive
 
-bot = commands.Bot(
-    command_prefix="!", 
-    case_insensitive=True
-)
+bot = commands.Bot(command_prefix="!", case_insensitive=True)
+
 
 @bot.event
 async def on_ready():
     print("Bot is ready")
+
 
 @bot.event
 async def on_member_join(member):
@@ -29,29 +33,42 @@ async def on_member_join(member):
     response = f"Welcome, {member.name}!"
     await channel.send(response)
 
+
 """
 @bot.event
 async def on_message(message):
-    if message.author == bot.user:
-        return
-    keywords = ["bingus", "plungus"]
-    channel = message.channel
-    for keyword in keywords:
-        if keyword.lower() in message.content.lower():
-            response = f"{message.author.mention} {keyword.lower()}"
+    response = ""
     await channel.send(response)
 """
 
+
 @bot.command()
 async def add(ctx, amount: typing.Optional[int] = 0):
-    
+
     if int(amount) > 0 and int(amount) < 10508:
+        if f"{ctx.message.author}" in db.keys():
+            db[f"{ctx.message.author}"] += amount
+        else:
+            db[f"{ctx.message.author}"] = amount
         await ctx.send(f"{amount} push-ups added to your total.")
     else:
         await ctx.send("Insufficient amount of push-ups.")
 
+
 @bot.command()
-async def bingus(ctx, num: typing.Optional[int] = 1, user: typing.Optional[str] = ""):
+async def total(ctx):
+    if f"{ctx.message.author}" in db.keys():
+        total = db[f"{ctx.message.author}"]
+    else:
+        db[f"{ctx.message.author}"] = 0
+        total = db[f"{ctx.message.author}"]  
+    await ctx.send(f"You have done {total} push-ups.")
+
+
+@bot.command()
+async def bingus(ctx,
+                 num: typing.Optional[int] = 1,
+                 user: typing.Optional[str] = ""):
 
     if num > 0 and num < 6 and user == "":
         for i in range(num):
@@ -65,7 +82,7 @@ async def bingus(ctx, num: typing.Optional[int] = 1, user: typing.Optional[str] 
     else:
         await ctx.send("come on now")
 
+
 keep_alive()
 token = os.environ['DISCORD_TOKEN']
 bot.run(token)
-
